430b87730972ca29845fe239ee4ebd5aa93df659
lucas <lucasgominhaa448@gmail.com>
2021-05-08 21:36:57 -0300

    added new changes in script.js

--- js/script.js
+++ js/script.js
@@ -30,39 +30,6 @@ main();
 
 
 
-/*function myScope(){
-    const form = document.querySelector('form');
-    const result = document.querySelector('.result');
-    
-    const people = [];
-
-    function receiveEventForm(event){
-
-        event.preventDefault();
-
-        const name = form.querySelector('.name');
-        const lastName = form.querySelector('.last-name');
-        const weight = form.querySelector('.weight');
-        const height = form.querySelector('.height');
-
-        people.push({
-            name: name.value,
-            lastName: lastName.value,
-            weight: weight.value,
-            height: height.value
-        });
-
-        console.log(people);
-
-        result.innerHTML += `<p>${name.value} ${lastName.value}` + ` ${weight.value} ${height.value}</p>`;
-        
-    }
-
-    form.addEventListener('submit',receiveEventForm);
-
-}
-
-myScope();*/
 
 
 
